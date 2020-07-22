3283c6a9bd66eb97f8735cfcd89f239beb2c7359
Luvhengo <tshilingadolph@gmail.com>
2020-07-22 04:01:21 +0200

    first commit

--- Patterns/Library.fs
+++ Patterns/Library.fs
@@ -2,6 +2,25 @@
 module Patterns
 #endif
 
+type Cell = 
+    |Positive
+    |Negative
+    |Unknown
+
+type Pattern =
+    |BlackP
+    |WhiteP
+    |UnknownP
+    |ZeroOrMore of Pattern
+    |OneOrMore of Pattern
+    |Exactly of int * Pattern
+    |FewerThan of int * Pattern
+    |Sequence of List<Pattern>
+    |Either of Pattern * Pattern
+    |Anything
+    |EndsOfCells
+
+
 let toCells v = failwith "Not implemented"
 
 let fromCells v = failwith "Not implemented" 
